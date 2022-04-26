c2290dfa6d4d5bdad2d17791b5068c3c87bb67a8
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-04-25 21:30:57 -0300

    feat: get data on database

--- todo.cpp
+++ todo.cpp
@@ -6,10 +6,120 @@ ToDo::ToDo(QWidget *parent)
     , ui(new Ui::ToDo)
 {
     ui->setupUi(this);
+    start();
+    db.setDatabaseName(userFile);// apos a funcao start ser executada nao sera mais o banco de dados setado no resource por isso setDatabaseNmae
+    if(db.open()){
+        show_data(); //se o databse estiver aberto ai chama o show_data , se nao comparar isso vai gerar erro,primeiro tem que estar ok banco para depois consultar
+    }
+    ui->table_list->setEditTriggers(QAbstractItemView::NoEditTriggers);
 }
 
 ToDo::~ToDo()
 {
     delete ui;
+}
+
+
+
+
+void ToDo::start(){
+     QFile file(userFile);
+
+     if(!file.exists()){
+         QFile::copy(dir,userFile);//estou copiando o banco de dados que esta no resource e envindo para HOME/.config/todo.db
+         system("chmod 600 ~/.config/todo.db");
+
+     }
+
+
+
+}
+
+
+void ToDo::show_data() {
+    QSqlQuery  query;
+    QString comand = "SELECT * FROM todos ORDER BY id DESC";
+    query.prepare(comand);
+
+
+    if(query.exec()){
+        int row = 0;
+        ui->table_list->setColumnCount(3);
+
+
+
+        while(query.next()){
+            ui->table_list->insertRow(row);
+
+            ui->table_list->setItem(row,0,new QTableWidgetItem(query.value(0).toString()));
+            ui->table_list->setItem(row,1,new QTableWidgetItem(query.value(1).toString()));
+            ui->table_list->setItem(row,2,new QTableWidgetItem(query.value(2).toString()));
+
+        }
+
+        QStringList headers {"ID", "DATE/TIME", "TASK"};
+
+        ui->table_list->setHorizontalHeaderLabels(headers);
+
+        ui->table_list->setColumnWidth(0, 50);
+        ui->table_list->setColumnWidth(1, 250);
+        ui->table_list->setColumnWidth(2, 750);
+
+        ui->table_list->verticalHeader()->setVisible(false);
+    }
+
+}
+
+
+void ToDo::on_action_out_triggered()
+{
+    this->close();
+}
+
+
+void ToDo::on_action_about_triggered()
+{
+   QMessageBox::about(this,"About program","About this project,<h2>ToDo 1.0.0</h2>"
+                                           "<p>Develop: Ricardo Kenji</p></br>"
+                                           "Copyright 2008-2020"
+                                           "<p><a href='https://github.com/kenjimaeda54/todo-QT'>Github</p></br>"
+                                           "<p>The program is <i> provided AS IS with NO WARRANTY OF ANY KIND,"
+                                           "INCLUDING THE WARRANTY OF DESING, MERCHANTABILITY"
+                                           "AND FITNESS FOR A PARTICULAR PORPOSE</i></p>");//para concatenar usar o ''
+}
+
+
+void ToDo::on_pushButton_clicked()
+{
+      QString task = ui->input_task->text();
+
+      if(task.isEmpty()){
+          QMessageBox::warning(this,"Attention","Can't save field empty");
+          return;
+
+      }
+
+      if(!db.open()){
+          QMessageBox::critical(this,"Error","I can`t open dabatabese");
+          return;
+
+      }
+
+      QSqlQuery query;
+      QString comand = "INSERT INTO todos( text  )  VALUES( '" +task+ "' )";
+      query.prepare(comand);
+
+      if(query.exec()){
+
+          ui->statusbar->showMessage("Data saved successfully");
+          QTimer::singleShot(2000,this,[&](){ui->statusbar->close();});//depois de 200 milisegundos vai fechar o statusbar;
+          ui->input_task->clear();
+          ui->input_task->setFocus();
+          ui->table_list->clear();  //para limpar oque esta visualmente na tela se nao vai replicar visualmente o valor anterior com uma nova consulta
+          show_data();
+
+      }
+
+
 }
 
--- todo.h
+++ todo.h
@@ -2,6 +2,11 @@
 #define TODO_H
 
 #include <QMainWindow>
+#include <QMessageBox>
+#include <QtSql>
+#include <QFileInfo>
+
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class ToDo; }
@@ -13,7 +18,22 @@ class ToDo : public QMainWindow
 
 public:
     ToDo(QWidget *parent = nullptr);
+
+    void start();  //essa funcao vai verificar se o caminho do user existe se nao exister vamos jogar o :/todo.db para o user,assim ele pode gerar um backup
+    QString pathHome = getenv("HOME");
+    QString dir = ":/todo.db";  //colocamos o database como source
+    QString userFile = pathHome + "/.config/todo.db"; //caminho que vai ficar backup do banco
+    void show_data();  //show e uma palavra reservada por isso show_data;
+
+    QSqlDatabase  db =  QSqlDatabase::addDatabase("QSQLITE");
     ~ToDo();
+
+private slots:
+    void on_action_out_triggered();
+
+    void on_action_about_triggered();
+
+    void on_pushButton_clicked();
 
 private:
     Ui::ToDo *ui;
